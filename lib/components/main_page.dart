530fdc1f8f7b659f9dd25a50c8ea25777778246a
abhishek <abhishek.mahajan314@gmail.com>
2020-01-20 09:27:30 +0530

    basic flare added

--- lib/components/main_page.dart
+++ lib/components/main_page.dart
@@ -1,4 +1,6 @@
 import 'package:dont_panic/components/default_main_page.dart';
+import 'package:dont_panic/components/earthquake_page.dart';
+import 'package:dont_panic/components/flood_page.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -9,6 +11,8 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   List calamities = [
     DefaultMainPage(),
+    EarthquakePage(),
+    FloodPage(),
   ];
   @override
   Widget build(BuildContext context) {
